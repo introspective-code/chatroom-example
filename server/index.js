01e50f3b579de85a9620dc819bf5840530d574b0
Nafeu Nasir <nafeu@tenjin.com>
2022-12-01 09:29:12 -0500

    Add nodemon and update server command

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 const server = http.Server(app);
